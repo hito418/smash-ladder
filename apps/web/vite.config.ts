e1b2be65badcd28eca758835cb3f2b093bde8fcb
hito418 <hito.wt@gmail.com>
2026-03-19 01:00:45 +0100

    refactor: use default import for TanStack router plugin

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,10 +1,10 @@
+import tanstackRouter from '@tanstack/router-plugin/vite'
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), TanStackRouterVite({ target: 'solid' }), solid()],
+  plugins: [tsconfigPaths(), tanstackRouter({ target: 'solid' }), solid()],
   server: {
     port: 3000,
     host: '0.0.0.0',
